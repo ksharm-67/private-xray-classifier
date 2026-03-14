dbd5764872009e209c2ead027e0b542cb02dcc65
Your Name <you@example.com>
2026-03-14 01:50:56 -0700

    added opacus for privacy

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.models import resnet50, ResNet50_Weights
 from torch.optim import Adam
+from opacus import PrivacyEngine
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -23,10 +24,19 @@ test_dl = DataLoader(ds_test, batch_size=64, shuffle=True)
 
 model = define_model()
 criterion = nn.BCEWithLogitsLoss()
-optimizer = Adam(model.parameters(), lr = 0.01)
+optimizer = Adam(model.parameters(), lr = 0.001)
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 model.to(device)
+
+pe = PrivacyEngine()
+model, optimizer, train_dl = pe.make_private(
+    module = model,
+    optimizer = optimizer,
+    data_loader = train_dl,
+    noise_multiplier = 1.1,
+    max_grad_norm = 1.0,
+    )
 
 def train(model: resnet50, train_dl: DataLoader, criterion) -> int:
     #Trains one epoch
@@ -57,6 +67,7 @@ def validate(model: resnet50, test_dl: DataLoader, criterion) -> int:
     
     with torch.no_grad():
         for ip, lab in test_dl:
+            ip, lab = ip.to(device), lab.to(device)            
             op = model(ip)
             loss = criterion(op, lab)
             
@@ -68,6 +79,7 @@ for epoch in range(100):
     rl = train(model, train_dl, criterion)
     vl = validate(model, test_dl, criterion)
     print(f"The running loss is = {rl} and the val loss is = {vl}")
+    torch.save(model.state_dict(), 'model.pth')
     
     
     
